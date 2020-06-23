27b887b34954e8d6104b7205b169fd92a2e82ede
Aseeva Julia <j.s.aseeva@mail.ru>
2020-06-23 17:51:21 +0300

    fix images info and fonts

--- src/components/Home/Partners/index.js
+++ src/components/Home/Partners/index.js
@@ -10,7 +10,7 @@ import partner6 from '../../../assets/img/projects/filmsclub-partners.png';
 import partner7 from '../../../assets/img/partners/blackcatcard-partner.png';
 import partner8 from '../../../assets/img/partners/vtb-partner.png';
 import partner9 from '../../../assets/img/partners/vera-id-partner.png';
-import partner10 from '../../../assets/img/partners/cryptica-bank-partner.png';
+import partner10 from '../../../assets/img/partners/amo-crm.png';
 import partner11 from '../../../assets/img/partners/buyex-partner.png';
 import partner12 from '../../../assets/img/partners/gratzbonus-partners.png';
 
@@ -20,19 +20,18 @@ export default function Partners() {
     <section className="section-partners">
       <h2 className="title title__partners">They trust us</h2>
       <div className="partners-image__list">
-        <Image src={partner1} alt='' classes='partners-image__item'/>
-        <Image src={partner2} alt='' classes='partners-image__item'/>
-        <Image src={partner3} alt='' classes='partners-image__item'/>
-        <Image src={partner4} alt='' classes='partners-image__item'/>
-        <Image src={partner5} alt='' classes='partners-image__item'/>
-        <Image src={partner6} alt='' classes='partners-image__item'/>
-        <Image src={partner7} alt='' classes='partners-image__item'/>
-        <Image src={partner8} alt='' classes='partners-image__item'/>
-        <Image src={partner9} alt='' classes='partners-image__item'/>
-        <Image src={partner10} alt='' classes='partners-image__item'/>
-        <Image src={partner11} alt='' classes='partners-image__item'/>
-        <Image src={partner12} alt='' classes='partners-image__item'/>
-
+        <Image src={partner1} alt='deltakredit' classes='partners-image__item'/>
+        <Image src={partner2} alt='glonas' classes='partners-image__item'/>
+        <Image src={partner3} alt='cashwagon' classes='partners-image__item'/>
+        <Image src={partner4} alt='royal-canin' classes='partners-image__item'/>
+        <Image src={partner5} alt='alcon' classes='partners-image__item'/>
+        <Image src={partner6} alt='filmsclub' classes='partners-image__item'/>
+        <Image src={partner7} alt='blackcatcard' classes='partners-image__item'/>
+        <Image src={partner8} alt='vtb' classes='partners-image__item'/>
+        <Image src={partner9} alt='vera-id' classes='partners-image__item'/>
+        <Image src={partner10} alt='amo-crm' classes='partners-image__item'/>
+        <Image src={partner11} alt='buyex' classes='partners-image__item'/>
+        <Image src={partner12} alt='gratzbonus' classes='partners-image__item'/>
       </div>
     </section>
   )
