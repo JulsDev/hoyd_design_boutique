5630a43cf462bde86adaef04739866b8d164cd1b
Aseeva Julia <j.s.aseeva@mail.ru>
2021-03-21 14:00:29 +0300

    update libs, fix scroll for portfolio

--- src/components/ProjectsList/index.js
+++ src/components/ProjectsList/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import data from '../../assets/projects.json';
 import ProjectCard from '../ProjectCard';
-
+import { HashLink } from 'react-router-hash-link';
 
 const tags = ['all', 'financial', 'blockchain', 'government', 'social'];
 
@@ -31,21 +31,26 @@ export default function ProjectList() {
       <h2 className="title title__cases">our cases</h2>
       
       <ul className="cases-tags__list">
-        {tags.map((item, index) => {          
+        {tags.map((item, index) => {
+          const hashItem = `#${item}`;          
           return (
             <li key={index}>
-              <button
-                type='button'
-                name={item}
-                onClick={handleChooseTag}
-                className={
-                  item === category ? 'cases-tags__item cases-tags__item--active' : 'cases-tags__item'
-                }  
-              >
-                <div className="cases-tags__link">
-                  {item}
-                </div>
-              </button>
+              <HashLink smooth to='#portfolio'>
+                <HashLink smooth to={hashItem}>
+                  <button
+                    type='button'
+                    name={item}
+                    onClick={handleChooseTag}
+                    className={
+                      item === category ? 'cases-tags__item cases-tags__item--active' : 'cases-tags__item'
+                    }  
+                  >
+                    <div className="cases-tags__link">
+                      {item}
+                    </div>
+                  </button>
+                </HashLink>
+              </HashLink>
             </li>
           )}
         )}  
